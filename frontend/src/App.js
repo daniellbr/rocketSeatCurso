6c7e6f82497f5e1932761ec9e0f17704809c4502
Daniel Bonifacio Romera <danielbr@cit>
2020-03-29 17:49:06 -0300

    Front inicio React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Biga na area logo logo to ai.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Header> 
+     Semana OmniStack!
+   </Header> // Como o header não tem nenhum conteudo 
+              //ela pode ser fechada nela mesma. 
+              //Agora para aplicar o conceito de propriedades
+              //passaremos a propriedade title="Semana OmniStack"
+              //Desta maneira o Header poderá ser utilizado em varias outras paginas
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; // Importando o React
+import ReactDOM from 'react-dom'; // Importando o React-Dom que é a arvore de elementos
+import App from './App'; // Importando App que é o arquivo App.js
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Um componente no react é uma função que retorna HTML
+//Ela pode ter funcionalidade JS CSS
+//Porem quando este HTML está escrito dentro do JS
+//Ele por definicção é chamado de JSX (JavaScript XML)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<App />, document.getElementById('root')); //* Esta linha de codigo quer dizer: 
+                                                           // Ele está utilizando o ReactDOM.render 
+                                                           //a variavel render é para REENDERIZAR na 
+                                                           //pagina em outras palavras é colocando em
+                                                           //Tela. Quanto o DOM está disponivel quer 
+                                                           //dizer que o HTML já está montado
+                                                           //Ele está colocando o App na tela
+                                                           //No REACT a variavel pode ser escrita igual
+                                                           //uma TAG HTML e a primeira letra é MAIUSCULA
+                                                           //E finalmente ele está colocando a variavel
+                                                           //App React dentro da div com o ID=root. E o que
+                                                           // tem dentro do App até o momento um 
+                                                           //<H1>Olá</H1>. Por isso
+                                                           // ele está dando um comando
+                                                           //document.getElementById('root'). Se no HTML 
+                                                           //estivesse escrito id=abobrinha ele iria colocar 
+                                                           //abobrinha no getElement.
+                                                             

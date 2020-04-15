399379143efc6cfbae0cb6d85f5f82f9803efe4f
Daniel Bonifacio Romera <danielbr@cit>
2020-04-14 21:15:59 -0300

    FrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import React from 'react';
+import './global.css';
+
+import Routes from './routes';
+
 
 function App() {
-  const [counter, setCounter] = useState(0)  ;
-
-  function increment(){
-    setCounter(counter + 1);
-  }
+ 
   return (
-   <div>
-     <header>Contador {counter}</header>
-     <button onClick={increment}>Incrementar</button>
-   </div>
+    <Routes />
   );
 }
 
